60170f809ea8bb5282aea4dc8c408025e36fa286
Chetan <chetan@vouchercodesuae.com>
2014-07-25 18:31:48 +0530

    Templating

--- LendLib.js
+++ LendLib.js
@@ -17,7 +17,7 @@ if (Meteor.isClient) {
   }
   Session.set('adding_category',false);
   
-  Template.categories.net_cat=function(){
+  Template.categories.new_cat=function(){
 	  return Session.equals('adding_category',true);
   };
 
@@ -26,8 +26,27 @@ if (Meteor.isClient) {
 		  Session.set('adding_category',true);
 		  Meteor.flush();
 		  focusText(t.find("#add-category"));
+	  },
+		
+	  'keyup #add-category':function(e,t){
+		  if(e.which===13){
+			  var catVal = String(e.target.value || "");
+			  if(catVal){
+				  lists.insert({Category:catVal});
+				  Session.set('adding_category',false);
+			  }
+		  }
+	  },
+		
+	  'focusout #add-category':function(e,t){
+		  Session.set('adding_category',false);
 	  }
   });
+
+  function focusText(i){
+	  i.focus();
+	  i.select();
+  }
 }
 
 if (Meteor.isServer) {
