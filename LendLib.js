a3569badf9242c38f3c9d233dc9aa11ec095ffbd
Chetan <chetan@vouchercodesuae.com>
2014-07-28 20:17:31 +0530

    Templating CRUD

--- LendLib.js
+++ LendLib.js
@@ -28,6 +28,10 @@ if (Meteor.isClient) {
 		  return "btn-inverse";
   };
 
+function selectCategory(e,t){
+	  console.log(this._id);
+	  Session.set('current_list',this._id);
+  }
   Template.categories.events({
 	  'click #btnNewCat':function(e,t){
 		  Session.set('adding_category',true);
@@ -56,9 +60,36 @@ if (Meteor.isClient) {
 	  i.select();
   }
 
-  function selectCategory(e,t){
-	  console.log(this._id);
-	  Session.set('current_list',this._id);
+  
+
+  function addItem(list_id,item_name){
+	  console.log(list_id+"  "+item_name);
+	  if(!item_name && !list_id)
+		  return;
+		  lists.update({_id:list_id},
+		  {$addToSet:{items:{Name:item_name}}});
+  }
+
+  function removeItem(list_id,item_name){
+	  console.log(list_id+"  "+item_name);
+	  if(!item_name && !list_id)
+		  return;
+		  lists.update({_id:list_id},
+		  {$pull:{items:{Name:item_name}}});
+  }
+
+  function updateLendee(list_id,item_name,lendee_name){
+	  var l = lists.findOne({"_id":list_id,"items.Name":item_name});
+	  if(l&&l.items){
+		  for(var i=0;i<l.items.length;i++)
+			{
+				if(l.items[i].Name === item_name)
+				{
+				  l.items[i].LentTo = lendee_name;
+				}
+			}
+		lists.update({"_id":list_id},{$set:{"items":l.items}});
+	  }
   }
 
   Template.list.items=function(){
@@ -66,9 +97,7 @@ if (Meteor.isClient) {
 		  return null;
 	  else{
 		  var cats = lists.findOne({_id:Session.get('current_list')});
-		  
 		  if(cats && cats.items){
-				
 			  for(var i=0;i<cats.items.length;i++){
 				  var d = cats.items[i];
 				  d.Lendee=d.LentTo?d.LentTo:"free";
@@ -80,7 +109,9 @@ if (Meteor.isClient) {
   }
 
   Template.list.list_selected=function(){
-	  return ((Session.get('current_lists')!=null)&&(!Session.equals('current_list',null)));
+	  
+	  return ((Session.get('current_list')!=null)&&(!Session.equals('current_list',null)));
+		  
   }
 
   Template.list.list_adding=function(){
@@ -96,6 +127,37 @@ if (Meteor.isClient) {
 		Session.set('list_adding',true);
 		Meteor.flush();
 		focusText(t.find('#item_to_add'));
+	  },
+	  'keyup #item_to_add':function(e,t){
+		  console.log(e.which);
+		  if(e.which===13){
+			  addItem(Session.get('current_list'),e.target.value);
+			  Session.set('list_adding',false);
+		  }
+	  },
+
+	  'focusout #item_to_add':function(e,t){
+		Session.set('list_adding',false);
+	  },
+	
+	  'click .delete_item':function(e,t){
+		  removeItem(Session.get('current_list'),e.target.id);
+	  },
+	
+	  'click .lendee':function(e,t){
+		  Session.set('lendee_input',this.Name);
+		  Meteor.flush();
+		  focusText(t.find("#edit_lendee"),this.LentTo);
+	  },
+
+	  'keyup #edit_lendee':function(e,t){
+		  if(e.which===13){
+			  updateLendee(Session.get('current_list'),this.Name,e.target.value);
+			  Session.set('lendee_input',null);
+		  }
+		  if(e.which===27){
+			  Session.set('lendee_input',null);
+		  }
 	  }
   });
 }
